18fc8480dab5b3835f66e5bf49b6986bfa6ed183
Csr-2 <2689139532@qq.com>
2026-09-16 14:35:16 +0800

    Last

--- src/main/java/com/test/student/StudentApplication.java
+++ src/main/java/com/test/student/StudentApplication.java
@@ -11,11 +11,6 @@ public class StudentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentApplication.class, args);
-        ApplicationContext ctx = SpringApplication.run(StudentApplication.class, args);
-
-        // 临时测试：看看有没有 RedisTemplate 这个 Bean
-        boolean hasRedis = ctx.containsBean("redisTemplate");
-        System.out.println("RedisTemplate是否存在: " + hasRedis);
     }
 
 }
